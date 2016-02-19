ee537ffc2ecb71879a9e6e2847c41b8ab106dabf
Adam Hundley <adamhundley@gmail.com>
2016-02-18 22:42:20 -0700

    added a few gems

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ActiveRecord::Base
     has_many :comments
     has_many :taggings
+    has_attached_file :image, styles: { medium: "300x300", thumb: "100x100" }
+    validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
     has_many :tags, through: :taggings
 
     def tag_list
@@ -10,6 +12,6 @@ class Article < ActiveRecord::Base
     def tag_list=(tags_string)
       tag_names = tags_string.split(",").map { |s| s.strip.downcase }.uniq
       new_or_found_tags = tag_names.map { |name| Tag.find_or_create_by(name: name) }
-      self.tags = new_or_found_tags 
+      self.tags = new_or_found_tags
     end
 end
